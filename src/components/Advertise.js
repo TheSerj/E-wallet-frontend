4bb084d52b1868ecf9421a79c3e92e531b2e23be
Dev Mittal <devkinandan.mittal.civ16@itbhu.ac.in>
2020-08-20 17:40:29 +0530

    rg

--- src/components/Advertise.js
+++ src/components/Advertise.js
@@ -3,16 +3,23 @@ import ewallet from '../images/e-wallet.jpg';
 import ewallet1 from '../images/e-wallet1.jpg';
 import ewallet2 from '../images/e-wallet2.jpg';
 import ewallet3 from '../images/e-wallet3.jpg';
-import {Row, Col} from 'reactstrap';
+import {Row, Col, Button} from 'reactstrap';
+import testAPI from '../utils/testAPI';
 
 class Advertise extends Component{
     constructor(props){
         super(props);
     }
-
+    handleApi(){
+        console.log("button clicked");
+        testAPI.get('/users').then((res)=>{
+            console.log(res.data[0].fullName);
+        })
+    }
     render(){
         const comp = (
             <div className="container text-center">
+                <Button onClick={this.handleApi}>Click this</Button>
                 <Row>
                     <Col xs="12" sm="6">
                         <img className="img-fluid img-thumbnail" src={ewallet1}/>
